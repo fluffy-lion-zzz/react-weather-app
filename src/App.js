d7e25febca8b86e6bb3af617089a4c679384db96
Fluffy-Lion <leoncooper31@gmail.com>
2021-07-05 22:28:51 +0100

    weather app start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+require('dotenv').config()
 
 function App() {
+  const [data, setData] = useState("")
+  const [error, setError] = useState({ error: false, message: "" })
+
+  
+  const handleFetch = async () => {
+    const key = process.env.REACT_APP_API_KEY
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&appid=${key}`)
+      
+      if(res.status !== 200){
+        throw new Error("oops")
+      }
+      const data = await res.json()
+      setData(data)
+      console.log(data)
+    } catch(error){
+      setError({ error: true, message: error.message})
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>quote</h1>
+      <button onClick={handleFetch}>fetch</button>
     </div>
   );
 }
