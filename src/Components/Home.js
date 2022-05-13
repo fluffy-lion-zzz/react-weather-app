a4cf294c13e30d52e6f327875613f8c1ea46b6ff
Fluffy-Lion <leoncooper31@gmail.com>
2022-05-13 11:38:17 +0100

    formatted/cleaner

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -27,38 +27,38 @@ const Home = ({ input, setInput, suggest, updateInput, handleFetch }) => {
   return (
     <div>
       <div className="imgWrapper">
-      {images.map((img, index) => {
-        return (
-          <img
-            src={img}
-            className={
-              index === currentSlide ? "imageActive homeImage" : "image"
-            }
-          />
-        );
-      })}
+        {images.map((img, index) => {
+          return (
+            <img
+              src={img}
+              className={
+                index === currentSlide ? "imageActive homeImage" : "image"
+              }
+            />
+          );
+        })}
       </div>
       <div className="content">
-      <h1>home</h1>
-      <input
-        className="userInput"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <ul>
-        {suggest &&
-          suggest.map((item, index) => {
-            return (
-              item !== null && (
-                <li key={index} onClick={() => updateInput(item)}>
-                  {item}
-                </li>
-              )
-            );
-          })}
-      </ul>
-      <h2>searching in the city: {input}</h2>
-      <button onClick={handleFetch}>fetch</button>
+        <h1>home</h1>
+        <input
+          className="userInput"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <ul>
+          {suggest &&
+            suggest.map((item, index) => {
+              return (
+                item !== null && (
+                  <li key={index} onClick={() => updateInput(item)}>
+                    {item}
+                  </li>
+                )
+              );
+            })}
+        </ul>
+        <h2>searching in the city: {input}</h2>
+        <button onClick={handleFetch}>fetch</button>
       </div>
     </div>
   );
