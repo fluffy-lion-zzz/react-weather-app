f94e00c25b0b775e25ae62674ec25998aa030f85
Fluffy-Lion <leoncooper31@gmail.com>
2022-06-21 15:18:11 +0100

    added breakpoint file

---
+++ src/Styles/Breakpoint.js
@@ -0,0 +1,11 @@
+const size = {
+    xs: "320px",
+    md: "425px",
+    lg: "768px"
+}
+
+const device = {
+    xs: `(min-width: ${size.xs})`,
+    md: `(min-width: ${size.md})`,
+    lg: `(min-width: ${size.lg})`
+}
