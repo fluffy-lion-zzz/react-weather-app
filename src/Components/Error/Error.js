973d30aecbc3e44264dc573e0b754c7d48dcf6e0
Fluffy-Lion <leoncooper31@gmail.com>
2022-08-10 13:02:11 +0100

    changed error styles path

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { ErrorContainer, EHeading } from "../../Styles/Styles";
+// import styled from "styled-components";
+import { ErrorContainer, EHeading } from "../../Styles/Styles.js";
 
 
 
